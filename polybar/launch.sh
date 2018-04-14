c4566c6c7781f045d055e9afae2d639ba168bd23
Ruven Salamon <salamon.ruven@gmail.com>
2018-04-14 18:32:14 +0200

    3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,8 +4,10 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+export MONITOR1=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | head -n 1)
+export MONITOR2=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
+
 polybar bar &
-polybar vga &
-polybar dp &
+polybar second &
 
 echo "Bars launched..."
